c5c727ef9415e7eb6705d5ff7952ca8222cf8dfe
Peter <113168584+prawstho@users.noreply.github.com>
2024-01-16 14:15:26 -0330

    enhnaced http file

--- http.js
+++ http.js
@@ -1,7 +1,8 @@
 var http = require('http');
 // var fs = require('fs');
 
-serveText('Hello World');
+//serveText('Hello World');
+serveHtml('<html><body><b>Woot</b> Woot</body></html>');
 
 function serveText(theText){
   http.createServer(function (req, res) {
@@ -9,6 +10,15 @@ function serveText(theText){
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write(theText);
     res.write(', eat more lunch');
+    res.end();
+  }).listen(3000);
+}
+
+function serveHtml(theHtml){
+  http.createServer(function (req, res) {
+    console.log('html was served.')
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(theHtml);
     res.end();
   }).listen(3000);
 }
