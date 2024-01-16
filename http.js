42ca78832ec6c8322bfd3b18a6692b5e8e9b2f63
Peter <113168584+prawstho@users.noreply.github.com>
2024-01-16 14:25:14 -0330

    reading disk files

--- http.js
+++ http.js
@@ -1,8 +1,10 @@
 var http = require('http');
-// var fs = require('fs');
+var fs = require('fs');
 
 //serveText('Hello World');
-serveHtml('<html><body><b>Woot</b> Woot</body></html>');
+//serveHtml('<html><body><b>Woot</b> Woot</body></html>');
+// serveFile('first.html');
+serveJson('user.json')
 
 function serveText(theText){
   http.createServer(function (req, res) {
@@ -20,5 +22,35 @@ function serveHtml(theHtml){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(theHtml);
     res.end();
+  }).listen(3000);
+}
+
+function serveFile(filename) {
+  http.createServer(function (req, res) {
+    fs.readFile(filename, function(err, data) {
+      if(err)
+          console.log(err);
+      else {
+        console.log('file ' + filename + ' was served.')
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      }   
+    });
+  }).listen(3000);
+}
+
+function serveJson(jsonfile) {
+  http.createServer(function (req, res) {
+    fs.readFile(jsonfile, function(err, data) {
+      if(err)
+          console.log(err);
+      else {
+        console.log('json file ' + jsonfile + ' was served.')
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.write(data);
+        return res.end();
+      }   
+    });
   }).listen(3000);
 }
